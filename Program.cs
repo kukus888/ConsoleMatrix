761a7e54390d912ce8389e6ea7fa831458066944
kukus888 <33528413+kukus888@users.noreply.github.com>
2018-01-01 16:18:30 +0100

    New version
    Added new shortcut:
    R key - run resizing

--- Program.cs
+++ Program.cs
@@ -11,97 +11,116 @@ namespace Matrix
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.ForegroundColor = ConsoleColor.Green;
 			int spaciDoba = 300;
+			int maximumPlaces = 750;
 			int vyskaKonzole = Console.LargestWindowHeight;
 			int sirkaKonzole = Console.LargestWindowWidth;
-			string[,] obrazovka = new string[sirkaKonzole,vyskaKonzole];
-			Random rnd = new Random();
+			string[,] obrazovka = new string[sirkaKonzole, vyskaKonzole];
+			Random rnd = new Random ();
 			int places = 0;
-			int maximumPlaces = 750;
-			maximumPlaces = ((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1))-((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1)/2);
+			maximumPlaces = ((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1)) - ((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1) / 2);
 			Console.Clear ();
-			try{
-			while (true) {
-				places = 0;
-				for (int i = 0; i <= obrazovka.GetLength (0) - 1; i++) {
-					for (int a = 0; a <= obrazovka.GetLength (1) - 1; a++) {
-						if (obrazovka [i, a] != null) {
-							places++;
-						}
-					}
-				}
-				if (places >= maximumPlaces) {
+			try {
+				while (true) {
+					places = 0;
 					for (int i = 0; i <= obrazovka.GetLength (0) - 1; i++) {
 						for (int a = 0; a <= obrazovka.GetLength (1) - 1; a++) {
-							obrazovka [i, a] = null;
+							if (obrazovka [i, a] != null) {
+								places++;
+							}
 						}
 					}
-					Console.Clear ();
-				}
-				for (int i = 0; i <= obrazovka.GetLength (0) - 1; i++) {
-					for (int a = 0; a <= obrazovka.GetLength (1) - 1; a++) {
-						if (obrazovka [i, a] != null) {
-							obrazovka [i, a] = rnd.Next (0,2).ToString();
+					if (places >= maximumPlaces) {
+						for (int i = 0; i <= obrazovka.GetLength (0) - 1; i++) {
+							for (int a = 0; a <= obrazovka.GetLength (1) - 1; a++) {
+								obrazovka [i, a] = null;
+							}
+						}
+						Console.Clear ();
+					}
+					for (int i = 0; i <= obrazovka.GetLength (0) - 1; i++) {
+						for (int a = 0; a <= obrazovka.GetLength (1) - 1; a++) {
+							if (obrazovka [i, a] != null) {
+								obrazovka [i, a] = rnd.Next (0, 2).ToString ();
+							}
 						}
 					}
-				}
-				if((vyskaKonzole != Console.LargestWindowHeight) || (sirkaKonzole != Console.LargestWindowWidth)){//RESIZE
-					obrazovka = null;
-					obrazovka = new string[Console.LargestWindowWidth,Console.LargestWindowHeight];
-					vyskaKonzole = Console.LargestWindowHeight;
-					sirkaKonzole = Console.LargestWindowWidth;
-					maximumPlaces = ((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1))-((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1)/2);
-					Console.Clear ();
-				}
-				int newLine = rnd.Next (0,100);
-				if (newLine >= 0 && newLine <= 49) {//50% chance of new line being generated
-					while (true) {
-						int x = rnd.Next (0, Console.LargestWindowWidth);
-						if (obrazovka [x, 0] == null) {
-							obrazovka [x, 0] = rnd.Next (0, 2).ToString ();
-							break;
-						}
+					if ((vyskaKonzole != Console.LargestWindowHeight) || (sirkaKonzole != Console.LargestWindowWidth)) {//RESIZE
+						obrazovka = null;
+						obrazovka = new string[Console.LargestWindowWidth, Console.LargestWindowHeight];
+						vyskaKonzole = Console.LargestWindowHeight;
+						sirkaKonzole = Console.LargestWindowWidth;
+						maximumPlaces = ((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1)) - ((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1) / 2);
+						Console.Clear ();
 					}
-				}
-				for (int z = 0; z <= obrazovka.GetLength(0)-1; z++) {//for every place
-					int evolveChance = rnd.Next (0,100);
-					bool obs = false;
-					if (obrazovka [z, 0] != null) {
-						obs = true;
-					}
-					if ((evolveChance >= 0 && evolveChance <= 84) && obs == true) {//90% chance to evolve
-						for(int m = 0;m<= obrazovka.GetLength(1)-1;m++){
-							if (obrazovka [z, m] == null) {
-								obrazovka[z,m]=rnd.Next(0,2).ToString();
+					int newLine = rnd.Next (0, 100);
+					if (newLine >= 0 && newLine <= 49) {//50% chance of new line being generated
+						while (true) {
+							int x = rnd.Next (0, Console.LargestWindowWidth);
+							if (obrazovka [x, 0] == null) {
+								obrazovka [x, 0] = rnd.Next (0, 2).ToString ();
 								break;
 							}
 						}
 					}
-				}
-				for (int i = 0; i <= obrazovka.GetLength (0) - 1; i++) {
-					for (int a = 0; a <= obrazovka.GetLength (1) - 1; a++) {
-						Console.SetCursorPosition (i, a);
-						Console.Write (obrazovka[i,a]);
+					for (int z = 0; z <= obrazovka.GetLength (0) - 1; z++) {//for every place
+						int evolveChance = rnd.Next (0, 100);
+						bool obs = false;
+						if (obrazovka [z, 0] != null) {
+							obs = true;
+						}
+						if ((evolveChance >= 0 && evolveChance <= 84) && obs == true) {//90% chance to evolve
+							for (int m = 0; m <= obrazovka.GetLength (1) - 1; m++) {
+								if (obrazovka [z, m] == null) {
+									obrazovka [z, m] = rnd.Next (0, 2).ToString ();
+									break;
+								}
+							}
+						}
 					}
-				}
-				if (Console.KeyAvailable == true) {
-					ConsoleKeyInfo vstup = Console.ReadKey ();
-					if (vstup.Key == ConsoleKey.Subtract) {
-						spaciDoba = spaciDoba - 50;
+					for (int i = 0; i <= obrazovka.GetLength (0) - 1; i++) {
+						for (int a = 0; a <= obrazovka.GetLength (1) - 1; a++) {
+							Console.SetCursorPosition (i, a);
+							Console.Write (obrazovka [i, a]);
+						}
 					}
-					if (vstup.Key == ConsoleKey.Add) {
-						spaciDoba = spaciDoba + 50;
+					if (Console.KeyAvailable == true) {
+						ConsoleKeyInfo vstup = Console.ReadKey ();
+						if (vstup.Key == ConsoleKey.Subtract) {
+							spaciDoba = spaciDoba - 50;
+						}
+						if (vstup.Key == ConsoleKey.Add) {
+							spaciDoba = spaciDoba + 50;
+						}
+						if (vstup.Key == ConsoleKey.R) {//Manually resize
+							obrazovka = null;
+							obrazovka = new string[Console.LargestWindowWidth, Console.LargestWindowHeight];
+							string systemOS = Environment.OSVersion.ToString ();
+							if(systemOS.Contains("Windows")) {
+								vyskaKonzole = Console.LargestWindowHeight-1;
+								sirkaKonzole = Console.LargestWindowWidth-1;
+								Console.SetWindowSize(sirkaKonzole,vyskaKonzole);
+							}
+							if(systemOS.Contains("Unix")) {
+								vyskaKonzole = Console.LargestWindowHeight;
+								sirkaKonzole = Console.LargestWindowWidth;
+							}
+							vyskaKonzole = Console.LargestWindowHeight;
+							sirkaKonzole = Console.LargestWindowWidth;
+							maximumPlaces = ((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1)) - ((obrazovka.GetLength (0) - 1) * (obrazovka.GetLength (1) - 1) / 2);
+							Console.Clear ();	
+						}
+						if (vstup.Key == ConsoleKey.Escape) {
+							Console.ResetColor();
+							break;
+						}
 					}
-					if (vstup.Key == ConsoleKey.Escape) {
-						break;
+					if (spaciDoba <= 0) {
+						spaciDoba = 1;
 					}
+					Thread.Sleep (spaciDoba);
 				}
-				if (spaciDoba <= 0) {
-					spaciDoba = 1;
-				}
-				Thread.Sleep (spaciDoba);
-			}
-			} catch {
-				
+			} catch(Exception e) {
+				Console.Write (e);
 			}
 		}
 	}
